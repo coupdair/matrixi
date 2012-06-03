740ad8dc97047dbccf825d2878dda836f06ec0c0
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2012-06-03 17:43:51 +0200

    working with LED diredtly connected to AP_47 and TTL in to AP_46. modified:   AVR.matrix.c: works (to do: try with AandDEE.shield.v0.1.1 )

--- AVR.matrix/AVR.matrix.c
+++ AVR.matrix/AVR.matrix.c
@@ -4,10 +4,11 @@
 #include <util/delay.h>
 #include <avr/io.h>
 
+//ArduinoMega pin 53-50,10-13 = PB0-7
 #define LED_DDR   DDRB
 #define LED_PORT PORTB
 #define LED_PIN   PINB
-//ArduinoMega pin 53-50,10-13 = PB0-7
+
 //16 bit = 2x8bit = PORTA and PORTC
 
 #define AP_13  PORTB7
@@ -19,10 +20,21 @@
 #define AP_52  PORTB1
 #define AP_53  PORTB0
 
+//ArduinoMega pin 41-49 = PL0-7 (PL1=48=SYNC, PL2=47=LED, PL3=46=IN)
+#define TTL_DDR   DDRL
+#define TTL_PORT PORTL
+#define TTL_PIN   PINL
+
+#define AP_46  PORTL3
+#define AP_47  PORTL2
+#define AP_48  PORTL1
+
 int main(void)
 {
   LED_DDR=255;//LED.matrix output
-  LED_DDR&=~_BV(AP_10);
+//  LED_DDR&=~_BV(AP_10);
+  TTL_DDR=255;//LED.matrix output
+  TTL_DDR&=~_BV(AP_46);
   char i,skip_nb=5;
   while(1)
   {
@@ -31,20 +43,27 @@ int main(void)
     LED_PORT|=_BV(AP_13);
 ///    LED_PORT|=_BV(AP_10);
     //wait for first TTL
-    loop_until_bit_is_set(LED_PIN,AP_10); //wait for PIV synchronization up
+//    loop_until_bit_is_set(LED_PIN,AP_10); //wait for PIV synchronization up
+    loop_until_bit_is_set(TTL_PIN,AP_46); //wait for PIV synchronization up
     LED_PORT&=~_BV(AP_13);
-    LED_PORT|=_BV(AP_12);
+//    LED_PORT|=_BV(AP_12);
+    TTL_PORT|=_BV(AP_47);
     //skip \c skip_nb TTL
-    loop_until_bit_is_clear(LED_PIN,AP_10); //wait for PIV synchronization down
+//    loop_until_bit_is_clear(LED_PIN,AP_10); //wait for PIV synchronization down
+    loop_until_bit_is_clear(TTL_PIN,AP_46); //wait for PIV synchronization down
     for(i=0;i<skip_nb;++i)
     {
       //toggle exposure LED (OFF/ON)
-      LED_PIN|=_BV(AP_12);
+//      LED_PIN|=_BV(AP_12);
+      TTL_PIN|=_BV(AP_47);
       //wait for next TTL
-      loop_until_bit_is_set(LED_PIN,AP_10); //wait for PIV synchronization up
-      loop_until_bit_is_clear(LED_PIN,AP_10); //wait for PIV synchronization down
+//      loop_until_bit_is_set(LED_PIN,AP_10); //wait for PIV synchronization up
+//      loop_until_bit_is_clear(LED_PIN,AP_10); //wait for PIV synchronization down
+      loop_until_bit_is_set(TTL_PIN,AP_46); //wait for PIV synchronization up
+      loop_until_bit_is_clear(TTL_PIN,AP_46); //wait for PIV synchronization down
     }//skip loop
-    LED_PORT&=~_BV(AP_12);
+//    LED_PORT&=~_BV(AP_12);
+    TTL_PORT&=~_BV(AP_47);
 //    LED_PORT=0xb00000000;
 //    LED_PORT=0;
 ///    LED_PORT&=~_BV(AP_10);
